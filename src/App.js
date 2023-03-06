bd5f1167f5b735e238496e6fb729d85b24193325
Elmo <elmotabuzojr.ust@gmail.com>
2023-03-06 14:56:24 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage />} />
+//     <Route path='/products' element={<ProductsPage />} />
+//   </Route>
+// );
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ],
+  },
+]);
+
+// const router = createBrowserRouter(routeDefinitions);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Products.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductsPage = () => {
+  return <h1>The products page</h1>;
+};
+
+export default ProductsPage;
